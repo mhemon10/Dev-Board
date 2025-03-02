69b5c3d37001f49b4cace2066953691e38871e1a
Emon <mhemon833@gmail.com>
2025-03-02 22:42:58 +0600

    my commit

--- js/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const backToDeskButton = document.getElementById("backToDeskButton");
-    backToDeskButton.addEventListener("click", function() {
-    window.location.href = "index.html";
-    });
-    });
